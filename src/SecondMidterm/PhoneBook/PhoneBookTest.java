cba3b7e4315f349b2b542db0755ba6e11f9eeb75
Aleksandar <jovanov.aleksandar997@gmail.com>
2021-01-09 02:34:27 +0100

    groupingBy example

---
+++ src/SecondMidterm/PhoneBook/PhoneBookTest.java
@@ -0,0 +1,126 @@
+package SecondMidterm.PhoneBook;
+
+import java.util.*;
+
+class DuplicateNumberException extends Exception {
+
+    public DuplicateNumberException(String number) {
+        super(String.format("Duplicate number: %s", number));
+    }
+}
+
+class Contact {
+    String name;
+    String number;
+    static Comparator<Contact> comparator = Comparator.comparing(Contact::getName)
+            .thenComparing(Contact::getNumber);
+
+    public Contact(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public List<String> getSubNumbers () {
+        List<String> results = new ArrayList<>();
+        for (int i=3;i<=number.length();i++) { //iterates the lengths of the subnumbers
+            for (int j=0; j<=number.length()-i;j++) {
+                results.add(number.substring(j, j+i));
+            }
+        }
+        return results;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + number;
+    }
+}
+
+
+class PhoneBook {
+    private Map<String, String> namesByPhoneNumbers;
+    private Map<String, Set<Contact>> contactsBySubNumbers;
+    private Map<String, Set<Contact>> contactsByName;
+
+    public PhoneBook() {
+        namesByPhoneNumbers = new HashMap<>();
+        contactsBySubNumbers = new HashMap<>();
+        contactsByName = new HashMap<>();
+    }
+
+
+    public void addContact(String name, String number) throws DuplicateNumberException {
+        if (namesByPhoneNumbers.containsKey(number))
+            throw new DuplicateNumberException(number);
+
+        namesByPhoneNumbers.put(number, name);
+
+        Contact c = new Contact(name, number);
+
+        c.getSubNumbers().forEach(
+                subnumber -> {
+                    contactsBySubNumbers.putIfAbsent(subnumber, new TreeSet<>(Contact.comparator));
+                    contactsBySubNumbers.get(subnumber).add(c);
+                }
+        );
+
+        contactsByName.putIfAbsent(name, new TreeSet<>(Contact.comparator));
+        contactsByName.get(name).add(c);
+    }
+
+    public void contactsByNumber(String number) {
+        if (contactsBySubNumbers.containsKey(number)) {
+            contactsBySubNumbers.get(number)
+                    .forEach(System.out::println);
+        } else {
+            System.out.println("NOT FOUND");
+        }
+    }
+
+    public void contactsByName(String name) {
+        if (contactsByName.containsKey(name)) {
+            contactsByName.get(name)
+                    .forEach(System.out::println);
+        } else {
+            System.out.println("NOT FOUND");
+        }
+    }
+}
+
+public class PhoneBookTest {
+
+    public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i < n; ++i) {
+            String line = scanner.nextLine();
+            String[] parts = line.split(":");
+            try {
+                phoneBook.addContact(parts[0], parts[1]);
+            } catch (DuplicateNumberException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            System.out.println(line);
+            String[] parts = line.split(":");
+            if (parts[0].equals("NUM")) {
+                phoneBook.contactsByNumber(parts[1]);
+            } else {
+                phoneBook.contactsByName(parts[1]);
+            }
+        }
+    }
+
+}
